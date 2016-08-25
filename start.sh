bf4df9df7c6b092e850eccd8153bb551af9753b1
Fahri YARDIMCI <ffahri@users.noreply.github.com>
2016-08-25 10:57:52 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ BINDIR=$(dirname "$(readlink -fn "$0")")
+ cd "$BINDIR"
+ java -Xmx2G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar spgt_ppr.jar
